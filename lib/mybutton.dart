fe2a4e6bcca5db768580298fdf3847986078b1a4
jcarter <Running-97-miles>
2021-05-12 13:55:51 -0700

    updated app

---
+++ lib/mybutton.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class MyButton {
+  bool visible;
+  String text;
+  Color color;
+
+  MyButton() {
+    visible = true;
+    text = '';
+    color = Colors.transparent;
+  }
+
+}
--- lib/story.dart
+++ lib/story.dart
@@ -1,5 +1,20 @@
-//TODO: Step 2 - Create a new class called Story.
 
-//TODO: Step 3 - Create 3 properties for this class, A. storyTitle to store the story text. B. choice1 to store the text for choice 1, C. choice2 to store the text for choice 2.
+class Story {
+  String storyTitle;
+  String choice1;
+  String choice2;
+  int next1;
+  int next2;
 
-//TODO: Step 4 - Create a Constructor for this class to be able to initialise the properties created in step 3.
+  Story( {String storyTitle, String choice1, String choice2, int next1, int next2 }) {
+    this.storyTitle = storyTitle;
+    this.choice1 = choice1;
+    this.choice2 = choice2;
+    this.next1 = next1;
+    this.next2 = next2;
+  }
+}
+
+// Step 2 - Create a new class called Story.
+// Step 3 - Create 3 properties for this class, A. storyTitle to store the story text. B. choice1 to store the text for choice 1, C. choice2 to store the text for choice 2.
+// Step 4 - Create a Constructor for this class to be able to initialise the properties created in step 3.
